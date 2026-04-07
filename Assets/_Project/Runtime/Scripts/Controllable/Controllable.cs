a082a8c489c5a65c0525dc250179ffdcc3ce3e2f
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-04-07 10:30:40 +0200

    [Mod] Events calling

--- Assets/_Project/Runtime/Scripts/Controllable/Controllable.cs
+++ Assets/_Project/Runtime/Scripts/Controllable/Controllable.cs
@@ -5,13 +5,49 @@ using UnityEngine.Events;
 
 namespace Metroma
 {
+    /// <summary>
+    /// Parent class of every Controllable.
+    /// You'll have to Inherit this class in order to modify its behavior.
+    /// </summary>
     public class Controllable : MonoBehaviour
     {
         private bool _isActive;
-        public bool IsActive { get => _isActive; set => _isActive = value; }
 
+        /// <summary>
+        /// Whether the Controllable captures inputs or not
+        /// </summary>
+        public bool IsActive
+        {
+            get => _isActive;
+            set {
+                _isActive = value;
+                
+                if (!_isActive) {
+                    _inputs = new GameplayInputsData();
+                }
+                else {
+                    // Check for AFK
+                }
+            }
+        }
+        
         protected GameplayInputsData _lastInputs;
-        public GameplayInputsData Inputs { get; set; }
+        protected GameplayInputsData _inputs;
+
+        public GameplayInputsData Inputs {
+            get => _inputs;
+            set
+            {
+                _lastInputs = _inputs;
+                _inputs = value;
+                
+                if (_lastInputs.move == Vector2.zero && Inputs.move != Vector2.zero) OnMoveStart?.Invoke();
+                else if (_lastInputs.move != Vector2.zero && Inputs.move == Vector2.zero) OnMoveEnd?.Invoke();
+            
+                if (_lastInputs.action == false && Inputs.action == true) OnActionStart?.Invoke();
+                else if (_lastInputs.action == true && Inputs.action == false) OnActionEnd?.Invoke();
+            }
+        }
         
         public Transform GetTransform { get => transform; }
         
@@ -20,15 +56,5 @@ namespace Metroma
 
         public UnityEvent OnActionStart;
         public UnityEvent OnActionEnd;
-
-        protected virtual void Update() {
-            if (_lastInputs.move == Vector2.zero && Inputs.move != Vector2.zero) OnMoveStart?.Invoke();
-            else if (_lastInputs.move != Vector2.zero && Inputs.move == Vector2.zero) OnMoveEnd?.Invoke();
-            
-            if (_lastInputs.action == false && Inputs.action == true) OnActionStart?.Invoke();
-            else if (_lastInputs.action == true && Inputs.action == false) OnActionEnd?.Invoke();
-
-            _lastInputs = Inputs;
-        }
     }
 }
