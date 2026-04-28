397e9e25adbf07cdba3b885f83600af0787f913f
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-04-28 15:17:27 +0200

    [Add] Je vais ta machoire dans le mur et utiliser tes dents comme porte-manteau

---
+++ Assets/_Project/Runtime/Scripts/Transitions/SpecificTransitions/ColorTransition.cs
@@ -0,0 +1,13 @@
+using Metroma.Transitions;
+using UnityEngine;
+
+namespace Metroma
+{
+    public class ColorTransition : ATransition
+    {
+        protected override Awaitable TransitionAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
