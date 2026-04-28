bfc275fb6b28c22f0a02f22b6d0d701e18db6370
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-04-28 15:27:49 +0200

    [Fix] Nan mais git il pu lààààà...

---
+++ Assets/_Project/Runtime/Scripts/Transitions/SpecificTransitions/ColorShapeTransition.cs
@@ -0,0 +1,23 @@
+using Metroma.Transitions;
+using Shapes;
+using UnityEngine;
+
+namespace Metroma
+{
+    public class ColorShapeTransition : ATransition
+    {
+        [SerializeField] private ShapeRenderer _Shape;
+        [SerializeField] private Color _targetColor;
+        [SerializeField] private float _duration;
+        
+        protected override Awaitable TransitionAsync() {
+            float timer = 0;
+            Color
+            while (timer < _duration) {
+                timer += Time.deltaTime;
+                
+                await Awaitable.FixedUpdateAsync();
+            }
+        }
+    }
+}
