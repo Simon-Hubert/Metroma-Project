8b90033bc206536f7476770fc3542b93dd6134f0
Simon-Hubert <s.hubert.mailpro@gmail.com>
2026-05-05 14:34:54 +0200

    [Add] smooth camcontroller

--- Assets/_Project/Runtime/Scripts/Camera/CameraController.cs
+++ Assets/_Project/Runtime/Scripts/Camera/CameraController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using static UnityEngine.Mathf;
 
 namespace Metroma
 {
@@ -7,9 +8,72 @@ namespace Metroma
     {
         [SerializeField] private Camera _cam;
         [SerializeField] private AView _view;
+        [SerializeField] private float _f = 1;
+        [SerializeField] private float _z = 2;
+        [SerializeField] private float _r = 0;
+        
+        private CameraConfiguration _current;
+        private SecondDegreeSmoother _smoother;
+        
+
+        private void Start() {
+            _smoother = new SecondDegreeSmoother(_f, _z, _r);
+        }
 
         private void Update() {
-            CameraHelpers.ApplyConfiguration(_cam, _view.GetConfiguration());
+            _current = _smoother.Smooth(_current, _view.GetConfiguration());
+            CameraHelpers.ApplyConfiguration(_cam, _current);
         }
     }
+    
+    
+    public class SecondDegreeSmoother
+    {
+        private CameraConfiguration _lastPos;
+        private CameraConfiguration _speed;
+
+        private Vector2 _lastYawVector;
+        private Vector2 _yawSpeed;
+    
+        private float _f, _z, _r; 
+        private float _k1, _k2, _k3;
+        
+        public SecondDegreeSmoother(float f, float z, float r) {
+            _f = f;
+            _z = z;
+            _r = r;
+            _k1 = _z / (PI * _f);
+            _k2 = 1 / ((2 * PI * _f) * (2 * PI * _f));
+            _k3 = _r * _z / (2 * PI * _f);
+        }
+
+        public CameraConfiguration Smooth(CameraConfiguration current, CameraConfiguration target) {
+        
+            CameraConfiguration lastSpeed = (target - _lastPos) / Time.deltaTime;
+            _lastPos = target;
+        
+            Vector2 yawVector =  new Vector2(
+                Cos(target.Yaw * Deg2Rad),
+                Sin(target.Yaw * Deg2Rad));
+        
+            Vector2 currentYawVector =  new Vector2(
+                Cos(target.Yaw * Deg2Rad),
+                Sin(target.Yaw * Deg2Rad));
+
+
+            Vector2 lastYawSpeed = (yawVector - _lastYawVector) / Time.deltaTime;
+            _lastYawVector = yawVector;
+        
+            _yawSpeed += Time.deltaTime * (yawVector + _k3 * lastYawSpeed - currentYawVector - _k1 * _yawSpeed) / _k2;
+        
+            current += Time.deltaTime * _speed;
+            _speed += Time.deltaTime * (target + _k3 * lastSpeed - current - _k1 * _speed) / _k2;
+
+            currentYawVector += _yawSpeed * Time.deltaTime;
+            current.Yaw = Atan2(currentYawVector.y, currentYawVector.x) * Rad2Deg;
+         
+            return current;
+        }
+    }
+
 }
--- Assets/_Project/Runtime/Scripts/Transitions/SpecificTransitions/SplineTransition.cs
+++ Assets/_Project/Runtime/Scripts/Transitions/SpecificTransitions/SplineTransition.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Dreamteck.Splines;
 using Metroma.Transitions;
 using UnityEngine;
@@ -10,15 +11,19 @@ namespace Metroma
         [SerializeField] private GameObject _object;
         [SerializeField] private float _duration;
         [SerializeField] SplineComputer _spline;
-
-
-        protected override async Awaitable TransitionAsync() {
-            float t = 0f;
-            while (t < _duration) {
-                await Awaitable.NextFrameAsync();
-                t += Time.deltaTime;
-                float p = t / _duration;
-                _object.transform.position = _spline.EvaluatePosition(p);
+        
+        protected override async Awaitable TransitionAsync(CancellationToken token) {
+            try {
+                float t = 0f;
+                while (t < _duration) {
+                    await Awaitable.NextFrameAsync();
+                    t += Time.deltaTime;
+                    float p = t / _duration;
+                    _object.transform.position = _spline.EvaluatePosition(p);
+                }
+            }
+            catch (OperationCanceledException oce) {
+                
             }
         }
     }
