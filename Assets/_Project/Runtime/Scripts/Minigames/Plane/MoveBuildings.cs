2bd00a4bf9ba047cc363f1796c3e6861298d4ae7
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-06-03 09:55:32 +0200

    [Mod] Transition Start setup

--- Assets/_Project/Runtime/Scripts/Minigames/Plane/MoveBuildings.cs
+++ Assets/_Project/Runtime/Scripts/Minigames/Plane/MoveBuildings.cs
@@ -18,8 +18,7 @@ namespace Metroma
     
     public class MoveBuildings : MonoBehaviour
     {
-        [SerializeField] private ButtonMashControllable _ctrl;
-        [SerializeField] private int _progressLimit;
+        [SerializeReference] private AnimCurveTransition _anim;
         [SerializeField] private float _speed;
         
         [SerializeField] private List<ParalaxeDecor> _paralaxes = new List<ParalaxeDecor>();
@@ -37,13 +36,13 @@ namespace Metroma
         }
 
         private void FixedUpdate() {
-            if (!_ctrl || !_ctrl.IsActive) return;
-
+            if (!_anim) return;
+            
             float delta = Time.fixedDeltaTime;
             for (int i = 0; i < _paralaxes.Count; i++) {
                 ParalaxeDecor decor = _paralaxes[i];
                 
-                decor.progression = Mathf.Repeat(decor.progression + (_speed * delta * decor.paralaxe * ((float)_ctrl.Value / _progressLimit)), decor.separation);
+                decor.progression = Mathf.Repeat(decor.progression + (_speed * delta * decor.paralaxe * _anim.GetValue), decor.separation);
                 decor.transform.position = decor.initPos + new Vector3(decor.progression, 0, 0);
 
                 _paralaxes[i] = decor;
