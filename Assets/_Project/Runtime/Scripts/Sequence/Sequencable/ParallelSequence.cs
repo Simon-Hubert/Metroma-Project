b073311f1757c11a2f55da7d3d4c8a8c53650222
Simon-Hubert <s.hubert.mailpro@gmail.com>
2026-06-10 14:53:21 +0200

    [Add] Visuels Parallel sequence

--- Assets/_Project/Runtime/Scripts/Sequence/Sequencable/ParallelSequence.cs
+++ Assets/_Project/Runtime/Scripts/Sequence/Sequencable/ParallelSequence.cs
@@ -17,11 +17,27 @@ namespace Metroma
         }
         
         public override async Awaitable ExecuteAsync() {
+            if (!RequirementsValidated()) return;
             foreach (ASequencable sequencable in _sequence) {
                 _ = sequencable.ExecuteAsync();
             }
 
             await Awaitable.WaitForSecondsAsync(_duration);
         }
+        
+        
+        public override bool RequirementsValidated() {
+            return transform.childCount > 0 && _duration > 0;
+        }
+        
+#if UNITY_EDITOR
+        public override InspectorColor GetInspectorColor() {
+            return new InspectorColor
+            {
+                Background = ColorHelpers.ColorFromOKLCH(BackgroundLightness, CHROMA, 0f),
+                Text = ColorHelpers.ColorFromOKLCH(TextLightness, CHROMA, 0f),
+            };
+        }
+#endif
     }
 }
