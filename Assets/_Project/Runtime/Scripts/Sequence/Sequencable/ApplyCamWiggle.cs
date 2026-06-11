d7088e6073a0d82f9294ee4f5e2cf597d74288b9
Simon-Hubert <s.hubert.mailpro@gmail.com>
2026-06-11 11:15:22 +0200

    [Add] CamWiggle Setter

---
+++ Assets/_Project/Runtime/Scripts/Sequence/Sequencable/ApplyCamWiggle.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Metroma
+{
+    public class ApplyCamWiggle : ASequencable
+    {
+        [SerializeField] private CamWiggleAnchor _anchor;
+        [SerializeField] private Vector3 _wiggleAmplitude;
+        [SerializeField] private bool _activate;
+
+        public override async Awaitable ExecuteAsync() {
+            if (!RequirementsValidated()) return;
+            _anchor.Target.enabled = _activate;
+            _anchor.Target.PositionWiggleProperties.AmplitudeMax = _wiggleAmplitude;
+        }
+        
+        public override bool RequirementsValidated() {
+            return _anchor;
+        }
+
+        private void OnValidate() {
+            if (!RequirementsValidated()) return;
+            name = $"Apply new wiggle to {_anchor.name}";
+        }
+        
+#if UNITY_EDITOR
+        public override InspectorColor GetInspectorColor() {
+            return new InspectorColor
+            {
+                Background = ColorHelpers.ColorFromOKLCH(BackgroundLightness, CHROMA, 140f),
+                Text = ColorHelpers.ColorFromOKLCH(TextLightness, CHROMA, 140f),
+            };
+        }
+#endif
+    }
+    }
+}
