c73e52b7e5901353836bc7412a7a0aae4c6d9291
Vanillou39 <lou.larbaletrier@gmail.com>
2026-06-12 11:49:28 +0200

    [ADD] Camera Shake Person

--- Assets/_Project/Runtime/Scripts/Anchors/CamWiggleSetter.cs
+++ Assets/_Project/Runtime/Scripts/Anchors/CamWiggleSetter.cs
@@ -5,10 +5,11 @@ namespace Metroma
 {
     public class CamWiggleSetter : MonoBehaviour
     {
+        [SerializeField] private MMWiggle _wiggle;
         [SerializeField] private CamWiggleAnchor _anchor;
 
         private void Awake() {
-            _anchor.Target = GetComponent<MMWiggle>();
+            _anchor.Target = _wiggle;
         }
     }
 }
