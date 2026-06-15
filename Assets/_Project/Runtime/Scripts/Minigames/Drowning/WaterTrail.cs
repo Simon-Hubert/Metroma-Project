158fe35f1835b4d0d60de9c4ded1094ae0542ec6
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-06-15 17:07:33 +0200

    [Mod] je fais ça plus tard

--- Assets/_Project/Runtime/Scripts/Minigames/Drowning/WaterTrail.cs
+++ Assets/_Project/Runtime/Scripts/Minigames/Drowning/WaterTrail.cs
@@ -11,10 +11,12 @@ namespace Metroma
         [SerializeField] private TrailRenderer _trail;
         [SerializeField] private float _startAlpha;
         [SerializeField] private float _endAlpha;
+        [Space(7)]
+        
 
         private void FixedUpdate()
         {
-            float alpha = Mathf.Lerp();
+            //float alpha = Mathf.Lerp();
             _trail.startColor = new Color(_trail.startColor.r, _trail.startColor.g, _trail.startColor.b, 0f);
         }
     }
