8da5a1ff2f79d3e4fccdb8efe6c2e01d0e512139
Remi Darmedru <remi.darmedru@outlook.com>
2026-06-23 13:21:51 +0200

    [Add] tous les sons et les evnets du sd

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,19 +17,73 @@ namespace AK
         static const AkUniqueID AMB_INTRO_INT_APPART_LP_STOP = 3721759591U;
         static const AkUniqueID AMB_MG_DRINK_LP_PLAY = 2060248577U;
         static const AkUniqueID AMB_MG_DRINK_LP_STOP = 85241303U;
-        static const AkUniqueID AMB_SUBWAY_2_PLAY = 3229337695U;
-        static const AkUniqueID AMB_SUBWAY_MOVING_LP_PLAY = 2531621026U;
-        static const AkUniqueID AMB_SUBWAY_MOVING_LP_STOP = 3133160444U;
-        static const AkUniqueID AMB_SUBWAY_STOP_LP_PLAY = 2898889856U;
-        static const AkUniqueID AMB_SUBWAY_STOP_LP_STOP = 1242148866U;
+        static const AkUniqueID AMB_MG_PLANE_AIR_LP_PLAY = 1405723464U;
+        static const AkUniqueID AMB_MG_PLANE_AIR_LP_STOP = 3783393722U;
+        static const AkUniqueID AMB_MG_PLANE_CITY_PLAY = 2363047086U;
+        static const AkUniqueID AMB_MG_PLANE_CITY_STOP = 3406162832U;
+        static const AkUniqueID AMB_MG_PLANE_ISLAND_LP_PLAY = 3946119335U;
+        static const AkUniqueID AMB_MG_PLANE_ISLAND_LP_STOP = 2681258229U;
+        static const AkUniqueID AMB_MG_POOL_LP_PLAY = 3536263191U;
+        static const AkUniqueID AMB_MG_POOL_LP_STOP = 2224705605U;
+        static const AkUniqueID AMB_SUBWAY_P1_PLAY = 4029900566U;
+        static const AkUniqueID AMB_SUBWAY_P1_STOP = 465470632U;
+        static const AkUniqueID AMB_SUBWAY_P2_PLAY = 3578410023U;
+        static const AkUniqueID AMB_SUBWAY_P2_STOP = 2313548917U;
+        static const AkUniqueID CIN_INTRO_PLAY = 1961391203U;
+        static const AkUniqueID MUS_SET_STATE_LOOP_1 = 2889129058U;
+        static const AkUniqueID MUS_SET_STATE_LOOP_2 = 2889129057U;
+        static const AkUniqueID MUS_SET_STATE_LOOP_3 = 2889129056U;
+        static const AkUniqueID MUS_SW_PLAY = 203146416U;
+        static const AkUniqueID MUS_SW_STOP = 2846595666U;
         static const AkUniqueID SFX_AD_TRANSITION_IN_PLAY = 3201995419U;
         static const AkUniqueID SFX_ALARM_CLOCK_PLAY = 2889854668U;
-        static const AkUniqueID SFX_GLASS_CHEER_PLAY = 4173467038U;
-        static const AkUniqueID SFX_GLASS_MOVE_PLAY = 1159349322U;
         static const AkUniqueID SFX_MG_DRINK_FIZZ_LP_PLAY = 2577013902U;
         static const AkUniqueID SFX_MG_DRINK_FIZZ_LP_STOP = 3578553200U;
+        static const AkUniqueID SFX_MG_DRINK_GLASS_CHEER_PLAY = 1081764954U;
+        static const AkUniqueID SFX_MG_DRINK_GLASS_MOVE_PLAY = 2791804670U;
+        static const AkUniqueID SFX_MG_POOL_KID_PLAY = 352263244U;
+        static const AkUniqueID SFX_MG_POOL_SWIM_LP_PLAY = 1498354397U;
+        static const AkUniqueID SFX_MG_POOL_SWIM_LP_STOP = 3684093259U;
+        static const AkUniqueID SFX_PLANE_CLOUD_HIT_PLAY = 2001426714U;
+        static const AkUniqueID SFX_PLANE_LAND_PLAY = 21179160U;
+        static const AkUniqueID SFX_PLANE_STARTING_PLAY = 1692706253U;
+        static const AkUniqueID SFX_PLANE_TAKE_OFF_PLAY = 1386668108U;
+        static const AkUniqueID SFX_POOL_SHARK_APPEAR_PLAY = 3585701352U;
         static const AkUniqueID SFX_SNOOZE_BUTTON_PLAY = 1797899227U;
+        static const AkUniqueID UI_HOVER_PLAY = 1131667641U;
+        static const AkUniqueID UI_PAUSE_IN_PLAY = 4134201843U;
+        static const AkUniqueID UI_PAUSE_OUT_PLAY = 788204342U;
+        static const AkUniqueID UI_RETURN_PLAY = 3063579953U;
+        static const AkUniqueID UI_SELECT_PLAY = 296772107U;
+        static const AkUniqueID UI_SLIDER_DOWN_PLAY = 3314553865U;
+        static const AkUniqueID UI_SLIDER_UP_PLAY = 2389629844U;
+        static const AkUniqueID VOC_POOL_CHILD_SCREAM_PLAY = 1779623446U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUS_SW
+        {
+            static const AkUniqueID GROUP = 1329476215U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LOOP_1 = 3987970511U;
+                static const AkUniqueID LOOP_2 = 3987970508U;
+                static const AkUniqueID LOOP_3 = 3987970509U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MUS_SW
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MASTER_VOL = 3578718921U;
+        static const AkUniqueID MUS_VOL = 1307889788U;
+        static const AkUniqueID PLANE_FLYING = 134457397U;
+        static const AkUniqueID SFX_VOL = 42448320U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
