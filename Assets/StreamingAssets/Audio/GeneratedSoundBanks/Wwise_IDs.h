65cf87ad627ba62a973dcbfdf0d98e955bfb2ec4
Renard Futile <75323641+TDupuisR@users.noreply.github.com>
2026-06-24 10:48:36 +0200

    [Mod] Je vais péter le crâne de ceux qui ont fait le changement sur la cinématic d'intro

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -77,14 +77,6 @@ namespace AK
 
     } // namespace STATES
 
-    namespace GAME_PARAMETERS
-    {
-        static const AkUniqueID MASTER_VOL = 3578718921U;
-        static const AkUniqueID MUS_VOL = 1307889788U;
-        static const AkUniqueID PLANE_FLYING = 134457397U;
-        static const AkUniqueID SFX_VOL = 42448320U;
-    } // namespace GAME_PARAMETERS
-
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
